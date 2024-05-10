4591b97e95afb4f15545bf51cc0ec62491411311
Wathrak <wathrak1@gmail.com>
2024-05-10 22:58:08 +0700

    added-fullscreen-mode

--- function.js
+++ function.js
@@ -4,3 +4,26 @@ function Toggle() {
   const list = document.getElementById("span-1");
   list.classList.toggle("run");
 }
+
+// Fullscreen
+var element = document.documentElement;
+let isFullscreen = false;
+
+function Fullscreen() {
+  if (isFullscreen == false) {
+    enterFullscreen();
+  }
+  else {
+    closeFullscreen();
+  }
+}
+
+function enterFullscreen() {
+    element.webkitRequestFullscreen();
+    isFullscreen = true;
+
+}
+function closeFullscreen() {
+    document.webkitExitFullscreen();
+    isFullscreen = false;
+}
