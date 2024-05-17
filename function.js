e8f658cb6d6d745930d16f172c38828c1cc1c1ee
SunLeang <leangkiyan2020@gmail.com>
2024-05-17 21:00:03 +0700

    Add button + fullscreen fixed

--- function.js
+++ function.js
@@ -1,4 +1,3 @@
-
 // Start & Stop Animation
 function Toggle() {
   const list1 = document.getElementById("div-1");
@@ -12,31 +11,51 @@ function Flicker() {
 }
 
 // Fullscreen
-var element = document.documentElement;
+let element = document.documentElement;
 let isFullscreen = false;
 let size = 300;
 
 function Fullscreen() {
-  if (isFullscreen == false) {
+  if (!isFullscreen) {
     enterFullscreen();
-  }
-  else {
+  } else {
     closeFullscreen();
   }
 }
-function enterFullscreen() {
-    element.webkitRequestFullscreen();
-    isFullscreen = true;
 
+function enterFullscreen() {
+  if (element.requestFullscreen) {
+    element.requestFullscreen();
+  } else if (element.mozRequestFullScreen) { // Firefox
+    element.mozRequestFullScreen();
+  } else if (element.webkitRequestFullscreen) { // Chrome, Safari and Opera
+    element.webkitRequestFullscreen();
+  } else if (element.msRequestFullscreen) { // IE/Edge
+    element.msRequestFullscreen();
+  }
+  isFullscreen = true;
+  document.body.classList.add('fullscreen');
 }
+
 function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.mozCancelFullScreen) { // Firefox
+    document.mozCancelFullScreen();
+  } else if (document.webkitExitFullscreen) { // Chrome, Safari and Opera
     document.webkitExitFullscreen();
-    isFullscreen = false;
+  } else if (document.msExitFullscreen) { // IE/Edge
+    document.msExitFullscreen();
+  }
+  isFullscreen = false;
+  document.body.classList.remove('fullscreen');
 }
+
 function fontIncrease() {
   size += 50;
   document.getElementById("p-1").style.fontSize = `${size}px`;
 }
+
 function fontDecrease() {
   size -= 50;
   document.getElementById("p-1").style.fontSize = `${size}px`;
@@ -45,21 +64,33 @@ function fontDecrease() {
 let x = 1;
 
 function switchStyle() {
-  var style1 = document.getElementById("style1");
-  var style2 = document.getElementById("style2");
-  var style3 = document.getElementById("style3");
+  const style1 = document.getElementById("style1");
+  const style2 = document.getElementById("style2");
+  const style3 = document.getElementById("style3");
   
-  if (x == 1) {
+  if (x === 1) {
     style1.href = "style2.css";
     x = 2;
-  }
-  else if (x == 2) {
+  } else if (x === 2) {
     style1.href = "style3.css";
     x = 3;
-  }
-  else if (x == 3) {
+  } else if (x === 3) {
     style1.href = "style1.css";
     x = 1;
   }
 }
 
+// Add event listeners for fullscreen changes
+document.addEventListener('fullscreenchange', onFullScreenChange);
+document.addEventListener('webkitfullscreenchange', onFullScreenChange);
+document.addEventListener('mozfullscreenchange', onFullScreenChange);
+document.addEventListener('MSFullscreenChange', onFullScreenChange);
+
+function onFullScreenChange() {
+  isFullscreen = !!(document.fullscreenElement || document.webkitFullscreenElement || document.mozFullScreenElement || document.msFullscreenElement);
+  if (isFullscreen) {
+    document.body.classList.add('fullscreen');
+  } else {
+    document.body.classList.remove('fullscreen');
+  }
+}
