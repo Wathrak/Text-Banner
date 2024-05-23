6cbd82869db61bcc267937276514a7c470f0bcdd
Wathrak <wathrak1@gmail.com>
2024-05-23 14:48:58 +0700

    x

--- index2.js
+++ index2.js
@@ -4,26 +4,65 @@ let element = document.documentElement;
 let isFullscreen = false;
 let size = 300;
 
+
 // Start & Stop Animation
 window.addEventListener("DOMContentLoaded", (event) => {
 
-    const animation_run = document.querySelector('#increase');
+    const div = document.getElementById('div-1');
+    const p = document.getElementById('p-1');
+
+    const animation_run = document.querySelector('#start-btn');
+    const fullscreen = document.querySelector('#fullscreen-btn');
+    const animationStyle = document.querySelector('#animation-style');
+    const fontStyle = document.querySelector('#font-style');
+    const font = document.querySelector('#font');
+    const color = document.querySelector('#coloring');
+
+
+    color.addEventListener('input', function() {
+        p.style.color = color.value;
+    });
+
+    font.addEventListener('change', function() {
+
+        if (font.value == 'polkadot') {
+            p.classList.remove('lobster');
+            p.classList.add('polkadot');
+            
+        }
+        else if (font.value == 'lobster') {
+            p.classList.remove('polkadot');
+            p.classList.add('lobster');
+        }
+    });
+
+    fontStyle.addEventListener('change', function() {
+
+        if (fontStyle.value == 'none') {
+            p.classList.remove('neon');
+            p.classList.remove('style-2');
+        }
+        else if (fontStyle.value == 'neon') {
+            p.classList.add('neon');
+            p.classList.remove('style-2');
+        }
+        else if (fontStyle.value == '') {
+            p.classList.add('style-2');
+            p.classList.remove('neon');
+        }
+        
+        
+    });
+
     animation_run.addEventListener('click', function() {
-       const a1 = getElementById('div-1');
-       a1.classList.toggle("run");
+        div.classList.toggle(animationStyle.value);
     });
 
-    const increase = document.querySelector('#increase');
-    increase.addEventListener('click', function() {
-        size += 50;
-        document.getElementById("p-1").style.fontSize = `${size}px`;
+    fullscreen.addEventListener('click', function() {
+        div.classList.add('run');
     });
 
-    const decrease = document.querySelector('#decrease');
-    decrease.addEventListener('click', function() {
-        size -= 50;
-        document.getElementById("p-1").style.fontSize = `${size}px`;
-    });
+
 
 });
 
