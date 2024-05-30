3bbbbd61d3313f4f938be0678bed754f3500657c
SunLeang <leangkiyan2020@gmail.com>
2024-05-30 22:04:39 +0700

    fix code align

--- index2.js
+++ index2.js
@@ -72,7 +72,8 @@ window.addEventListener("DOMContentLoaded", (event) => {
 
   // Font Family
   font.addEventListener("change", function () {
-    const fonts = ["polkadot", "lobster", "montserrat", "courier", "franklin", "jacquard", "dancingScript", "danfo", "jacquard24", "shadows-into-light"];
+    const fonts = ["polkadot", "lobster", "montserrat", "courier", "franklin", 
+                    "jacquard", "dancingScript", "danfo", "jacquard24", "shadows-into-light"];
     fonts.forEach(font => p.classList.remove(font));
     if (font.value !== "1") {
       p.classList.add(font.value);
