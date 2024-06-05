841a5180d87a7ab4a7cf0b19bc63866ce768325c
SunLeang <leangkiyan2020@gmail.com>
2024-06-05 09:54:02 +0700

    add comment

--- index2.js
+++ index2.js
@@ -231,11 +231,11 @@ function addSnowflakes() {
     // Adjust the number of snowflakes as needed
     const snowflake = document.createElement("div");
     snowflake.classList.add("snowflake");
-    snowflake.style.left = Math.random() * 100 + "vw";
-    snowflake.style.animationDuration = Math.random() * 3 + 2 + "s";
+    snowflake.style.left = Math.random() * 100 + "vw"; // Random horizontal position
+    snowflake.style.animationDuration = Math.random() * 3 + 2 + "s"; // Random fall duration
     snowflake.style.opacity = Math.random();
     snowflake.style.width = snowflake.style.height =
-      Math.random() * 7 + 3 + "px";
+      Math.random() * 7 + 3 + "px"; // Random size between 3px and 10px
     snowContainer.appendChild(snowflake);
   }
 }
